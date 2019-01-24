d37b163a64ef4965f05552123161bf56999d6acd
Amanda <aeverett1317@gmail.com>
2019-01-24 08:04:00 -0700

    created functional CartFooter component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header.js'
+import CartFooter from './components/CartFooter.js'
+
 
 class App extends Component {
   render() {
     return (
-     <Header />
-    );
+     <body>
+       <Header />
+      <CartFooter />
+     </body>
+    )
   }
 }
 
